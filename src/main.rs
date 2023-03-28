7052193cdd32bd8cab8da72015953cb3ad542aad
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2023-03-27 20:52:00 -0700

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,26 @@ use clap::Parser as ClapParser;
 use notify::RecursiveMode;
 use notify_debouncer_mini::{new_debouncer, DebouncedEvent, DebouncedEventKind};
 use std::{
-    path::{Path, PathBuf},
+    path::{Path, PathBuf, MAIN_SEPARATOR_STR},
     process::Command,
     time::Duration,
 };
+
+/// Just for displaying paths as strings
+macro_rules! p {
+    ($path:expr) => {
+        $path.to_string_lossy()
+    };
+}
 
 #[derive(ClapParser, Debug)]
 #[command(
     author = "Jonxslays",
     version = "0.1.0",
-    about = "A ziglings file watcher."
+    about = "Zwatch - A ziglings hot reloader."
 )]
 struct Parser {
     /// The path to your ziglings directory
-    #[arg(short, long)]
     path: String,
 }
 
@@ -52,7 +58,7 @@ fn rebuild_file(path: &mut PathBuf) {
     let target = path
         .file_name()
         .unwrap_or_else(|| {
-            eprintln!("Invalid file name for path {}", path.to_string_lossy());
+            eprintln!("Invalid file name for path {}", p!(path));
             std::process::exit(1);
         })
         .to_string_lossy();
@@ -63,63 +69,69 @@ fn rebuild_file(path: &mut PathBuf) {
         std::process::exit(1);
     });
 
-    path.pop();  // Yes
-    path.pop();  // Also, yes
+    path.pop(); // Yes
+    path.pop(); // Also, yes
 
     Command::new("zig")
         .arg("build")
         .arg("--build-file")
-        .arg(&format!("{}/{}", path.to_string_lossy(), "build.zig"))
+        .arg(&format!("{}{}build.zig", p!(path), MAIN_SEPARATOR_STR))
         .arg(exercise.to_string())
-        .spawn()  // :) have a great day!
+        .spawn() // :) have a great day!
         .unwrap_or_else(|err| {
-            eprintln!("Error: Do you have zig installed? - {}", err.to_string());
+            eprintln!("Do you have zig installed? - {}", err.to_string());
             std::process::exit(1);
         });
+}
+
+fn handle_err_events(errs: Vec<notify::Error>) {
+    let message = errs
+        .iter()
+        .map(|e| e.to_string())
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    eprintln!("{}", message);
+}
+
+fn handle_message(message: Result<Vec<DebouncedEvent>, Vec<notify::Error>>) {
+    match message {
+        Ok(events) => handle_file_events(events),
+        Err(errs) => handle_err_events(errs),
+    }
+}
+
+fn prepare_path(path: &Path) -> PathBuf {
+    let mut path = path.canonicalize().unwrap_or_else(|err| {
+        println!("{}", err.to_string());
+        println!("Targeting: {}", p!(path));
+        std::process::exit(1);
+    });
+
+    path.extend(["exercises"]);
+
+
+    if !path.exists() {
+        path.pop();
+        eprintln!("Cannot find ziglings exercises directory in {}", p!(path));
+        std::process::exit(1);
+    }
+
+    path
 }
 
 fn main() -> notify::Result<()> {
     let parser = Parser::parse();
     let (tx, rx) = std::sync::mpsc::channel();
-    let mut path = Path::new(&parser.path)
-        .canonicalize()
-        .unwrap_or_else(|err| {
-            println!("Error: {}", err.to_string());
-            println!("Targeting: {}", parser.path);
-            std::process::exit(1);
-        });
-
-    path.extend(["exercises"]);
-    if !path.exists() {
-        path.pop();
-
-        eprintln!(
-            "Cannot find ziglings exercises directory in {}",
-            path.to_string_lossy(),
-        );
-
-        std::process::exit(1);
-    }
-
+    let path = prepare_path(Path::new(&parser.path));
     let mut debouncer = new_debouncer(Duration::from_secs(2), None, tx)?;
-    println!("Watching for file changes in {}", path.to_string_lossy());
 
     #[rustfmt::skip]
     debouncer.watcher().watch(path.as_path(), RecursiveMode::NonRecursive)?;
+    println!("Watching for file changes in {}", p!(path));
 
     for message in rx {
-        match message {
-            Ok(events) => handle_file_events(events),
-            Err(errs) => {
-                let message = errs
-                    .iter()
-                    .map(|e| e.to_string())
-                    .collect::<Vec<String>>()
-                    .join("\n");
-
-                eprintln!("Error: {}", message);
-            }
-        }
+        handle_message(message);
     }
 
     Ok(())
